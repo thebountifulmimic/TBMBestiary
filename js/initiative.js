a560e77b32bd7f2646bd22e7e26c0b88c2ffca12
Devin <dngrenard@gmail.com>
2025-09-09 17:00:02 -0400

    Expanding the panels correctly

--- js/initiative.js
+++ js/initiative.js
@@ -466,7 +466,7 @@ document.querySelectorAll('.resizer').forEach((handle) => {
 
     // Lock both panels to explicit pixel widths so they don't switch to auto
     left.style.flex = `0 0 ${newLeft}px`;
-    right.style.flex = `0 0 ${newRight}px`;
+    right.style.removeProperty('flex');
   };
 
   const onMouseUp = () => {
