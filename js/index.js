f211f1bc1cc23ea7689f9bc0da06e3e4f0e0aebf
thebountifulmimic <thebountifulmimic@gmail.com>
2025-10-26 15:41:52 -0400

    Index.js correct URL

--- js/index.js
+++ js/index.js
@@ -184,7 +184,7 @@ async function loadMonsters() {
 
         const li = document.createElement("div");
         li.className = "monster-link";
-        li.innerHTML = `<a href="monster.html?file=${encodeURIComponent(m._file)}">${m._displayName || m.name || m._file}</a>`;
+        li.innerHTML = `<a href="/${encodeURIComponent(m._file.replace('.json', ''))}">${m._displayName || m.name || m._file}</a>`;
         listEl.appendChild(li);
       });
     }
@@ -198,6 +198,7 @@ async function loadMonsters() {
     console.log("Finished attempting to load monsters");
   }
 }
+
 
 
 
